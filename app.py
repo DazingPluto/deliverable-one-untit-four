2d4b78f39654ac8e73320ff9fad7c8b42f8cc3fb
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2022-01-13 19:00:57 -0600

    deliverable in class work

---
+++ app.py
@@ -0,0 +1,55 @@
+# print(4 + 4 / 2)
+
+# print((2 + -7) *3)
+
+# print(101 % 10)
+
+# print(12 - 4 % 3)
+
+# print (True and False)
+
+# print(True and not(False or False))
+
+# print(not True and not(False or False))
+
+# print('cat' + 'dog')
+
+# print( str(2) + 'pizza')
+
+# print( str(2.5) * 'fish')
+
+# string = 'fish'
+
+# print(string * 2)
+
+# print(5 >= 11)
+
+# print(5 == 5.0)
+
+# print(7 != 7.1)
+
+# print(5 + 5 > 8)
+
+# print(6 + 6 != 12)
+
+# print(2 > 1 or False)
+
+# print('true' == True)
+
+# print(10 % 2 == 0)
+
+# print(21 & 2 == 0)
+
+# print(21 % 2 != 0)
+
+# print(21 % 2 == 1)
+
+# print(12 % 3 == 0)
+
+# print(9 % 3 == 0)
+
+# print(14 % 3 == 0)
+
+# print('newyork'[0])
+
+# print('san franscisco'[2 * 3])
